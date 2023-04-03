64e8833c3c2760fbef7f4462cfbfa00e77293fdd
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-03 09:31:42 +0300

    Cucumber: TEst Runner Class ExtendReport 1

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,6 +14,11 @@ public class GWD {
 
     public static WebDriver getDriver()
     {
+        // extend report türkçe bilg çalışmaması sebebiyle kondu
+        Locale.setDefault(new Locale("EN"));
+        System.setProperty("user.language", "EN");
+
+
         Logger logger= Logger.getLogger("");
         logger.setLevel(Level.SEVERE);
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
