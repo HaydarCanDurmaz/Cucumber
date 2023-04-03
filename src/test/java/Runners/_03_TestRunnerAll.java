e357d8482e29a4b6c77a22c28fa8cd3944f1dc58
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-03 08:43:08 +0300

    Cucumber: TEst Runner Class dryRun : true , false

--- src/test/java/Runners/_03_TestRunnerAll.java
+++ src/test/java/Runners/_03_TestRunnerAll.java
@@ -5,7 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        dryRun = true // true oldugunda testi çalıştırmaz sadece feature lra
+                      // ait steplerin varlıgını kontrol eder
+                      // fase oldugunda ise testi ÇALIŞTIRIR
 )
 public class _03_TestRunnerAll extends AbstractTestNGCucumberTests {
 }
