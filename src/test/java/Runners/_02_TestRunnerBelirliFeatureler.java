417298a2c9dd7f845dd2908280bdfd81dd28c337
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-03 08:19:31 +0300

    Cucumber: TEst Runner 2

---
+++ src/test/java/Runners/_02_TestRunnerBelirliFeatureler.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {
+                "src/test/java/FeatureFiles/_01_Login.feature",
+                "src/test/java/FeatureFiles/_02_Country.feature"},
+        glue = {"StepDefinitions"}
+)
+public class _02_TestRunnerBelirliFeatureler extends AbstractTestNGCucumberTests {
+}
