91f6449787446b35107d9b7531bd2686d09572be
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-18 09:12:59 +0300

    JBDC - JBDCParent- Soru

--- src/test/java/_JBDC/Gun1/_03_Soru.java
+++ src/test/java/_JBDC/Gun1/_03_Soru.java
@@ -1,5 +1,26 @@
 package _JBDC.Gun1;
 
-public class _03_Soru {
+import _JBDC.JBDCParent;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _03_Soru extends JBDCParent {
+    // city tablosundaki tüm kayıtlardaki isimleri next ile yazdırınız
+    @Test
+    public  void test1() throws SQLException {
+        ResultSet rs = statement.executeQuery("select  city from city");
+
+        int rowCount=0;
+        while (rs.next()==true){
+            System.out.println(rs.getString("city"));
+            rowCount++;
+        }
+        System.out.println("rowCount = " + rowCount);
+
+    }
+    // TODO : city tablosundaki tüm satırlardaki şehir isimlerini absolute ile yazdırınız
+    // TODO : city tablosundaki tüm satırlardaki şehir isimlerini relative ile yazdırınız
 
 }
