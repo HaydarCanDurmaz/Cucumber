2fc0dbe252a3f91227e61058c5fec2c68aa85e76
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-19 15:44:43 +0300

    JBDC - JBDCParent- _01_GetAllRowColumn test2

--- src/test/java/_JBDC/Gun2/_01_GetAllRowColumn.java
+++ src/test/java/_JBDC/Gun2/_01_GetAllRowColumn.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class _01_GetAllRowColumn extends JBDCParent {
     @Test
-    public  void test1() throws SQLException {
+    public void test1() throws SQLException {
         ResultSet rs = statement.executeQuery("select * from language");
 
         ResultSetMetaData rsmd = rs.getMetaData();
@@ -22,5 +22,27 @@ public class _01_GetAllRowColumn extends JBDCParent {
             System.out.print("Column name=" + rsmd.getColumnName(i));
             System.out.println("\tColumn type=" + rsmd.getColumnTypeName(i));
         }
+    }
+    @Test
+    public void test2() throws SQLException {
+        // language tablosundaki tüm satırları ve tüm sütunları yazdırınız,
+        // aynı mysql sonuç ekranında olduğu gibi
+
+        ResultSet rs = statement.executeQuery("select * from language"); // data + metdata
+        ResultSetMetaData rsmd = rs.getMetaData(); // data dışındaki bilgileri: kolon sayısı,ismi,tipleri
+
+        for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+            System.out.print(rsmd.getColumnName(i) + "\t");
         }
+        System.out.println();
+
+        while (rs.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                System.out.print(rs.getString(i) + "\t");
+            }
+
+            System.out.println();
+        }
+
+    }
 }
