947ae8a28a9a5c2861d3ffd74038dc2425784753
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-05-19 20:05:36 +0300

    plugin

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -5,10 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 import org.checkerframework.checker.units.qual.C;
 
 @CucumberOptions(
-        tags = "@SmokeTest", // hangi Senaryolarda bu etiket varsa çalışır
+        tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
 )
-
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
